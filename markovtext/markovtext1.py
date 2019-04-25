87f85f4493c8dc50e92046f28ecd43c5cc156134
Pimski26 <p.w.j.terietmole@students.uu.nl>
2019-04-25 16:03:29 +0200

    Niet-werkende maar hopelijk betere versie v1

--- markovtext/markovtext1.py
+++ markovtext/markovtext1.py
@@ -1,21 +1,34 @@
 import random
+from collections import Counter
 
-text = """What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class
-       in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills.
-        I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but 
-       just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on
-        this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? 
-       Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being 
-       traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing
-        you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven
-        hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but 
-       I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe 
-       your miserable ass off the face of the continent, you little shit. If only you could have known what unholy 
-       retribution your little “clever” comment was about to bring down upon you, maybe you would have held your 
-       fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit 
-       fury all over you and you will drown in it. You’re fucking dead, kiddo."""
+dictall = {}
+dictstart = {}
 
-wordlist = text.split(' ')
+with open('navysealcopypasta.txt','r') as file:
+
+    for line in file:
+        wordlist = line.split(' ')
+        # Als er laatste regel een woord geskipt is, voeg hem hier alsnog toe in de dictionary
+        if lastword:
+            dictall.setdefault(lastword, Counter())
+            dictall[lastword][wordlist[0]] += 1
+        # Voeg alle woorden van deze line toe aan de dictionary
+        for idx, word in enumerate(wordlist):
+            dictall.setdefault(word, Counter())
+            if idx < wordlist.__len__():
+                # Next word exists so just do normal stuff
+                dictall[word][wordlist[idx+1]] += 1
+            else:
+                # End of line; oh oh
+                lastword = word
+
+
+
+
+
+#wordlist = text.split(' ')
+
+
 dict = {}
 for x in range(len(wordlist)-1):
     dict.setdefault(wordlist[x], [])
