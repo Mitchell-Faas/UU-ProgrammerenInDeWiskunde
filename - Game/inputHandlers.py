c537ddeb74de3375dc13c38eebbf002497bcd678
Mitchell Faas <Faas.Mitchell@gmail.com>
2019-04-29 13:04:21 +0200

    Added player character and basic movement/drawing logic.

---
+++ - Game/inputHandlers.py
@@ -0,0 +1,23 @@
+import tcod
+
+
+def handleKeys(key):
+    action_dict = { # Defines return values for single-key actions
+                   tcod.KEY_UP : {'move': (0, -1)},
+                   tcod.KEY_DOWN : {'move': (0, 1)},
+                   tcod.KEY_LEFT : {'move': (-1, 0)},
+                   tcod.KEY_RIGHT : {'move': (1, 0)},
+                   tcod.KEY_ESCAPE : {'exit': True}
+                   }
+
+    # Multi-key actions
+    if key.vk == tcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle full screen
+        return {'fullscreen': True}
+
+    try:
+        # Return the appropriate action
+        return action_dict[key.vk]
+    except KeyError:
+        # Key is unkown.
+        return {}
