33c8e0ea32da3e40bf02e89edde492f1c9909d2a
Pimski26 <p.w.j.terietmole@students.uu.nl>
2019-04-30 11:06:34 +0200

    Merge branch 'master' of https://github.com/Mitchell-Faas/UU-ProgrammerenInDeWiskunde
    # Conflicts:
    #	- Game/renderFunctions.py

--- - Game/renderFunctions.py
+++ - Game/renderFunctions.py
@@ -1,25 +1,22 @@
 import tcod
 
 
-def render_all(console, entities, screen_width, screen_height):
-    """Renders all given entities on the screen.
+def render_all(console, entities, game_map, screen_width, screen_height, colours):
+    # Draw the tiles in game_map
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].block_sight
 
-    params
-    -------------
-    console: console object
-    entities: list of entity objects
-    screen_width: horizontal length of the screen in number of tiles
-    screen_height: vertical length of the screen in number of tiles
+            if wall:
+                tcod.console_set_char_background(console, x, y, colours.get('dark_wall'), tcod.BKGND_SET)
+            else:
+                tcod.console_set_char_background(console, x, y, colours.get('dark_ground'), tcod.BKGND_SET)
 
-    returns
-    -------------
-    None
-    """
     # Draw all entities in the list
     for entity in entities:
         draw_entity(console, entity)
 
-    tcod.console_blit(console=console,
+    tcod.console_blit(src=console,
                       x=0, y=0,
                       w=screen_width,
                       h=screen_height,
@@ -27,32 +24,11 @@ def render_all(console, entities, screen_width, screen_height):
                       xdst=0, ydst=0)
 
 def clear_all(console, entities):
-    """Removes all given entities from screen.
-
-    params
-    -------------
-    console: console object
-    entities: list of entity objects
-
-    returns
-    -------------
-    None"""
     for entity in entities:
         clear_entity(console, entity)
 
 
 def draw_entity(console, entity):
-    """Renders a given entity on the screen.
-
-    params
-    -------------
-    console: console object
-    entity: entity object
-
-    returns
-    -------------
-    None
-    """
     tcod.console_set_default_foreground(con=console, col=entity.colour)
     tcod.console_put_char(con=console,
                           x=entity.x, y=entity.y,
@@ -61,17 +37,6 @@ def draw_entity(console, entity):
 
 
 def clear_entity(console, entity):
-    """Removes a given entity from the screen.
-
-      params
-      -------------
-      console: console object
-      entity: entity object
-
-      returns
-      -------------
-      None
-      """
     # erase the character that represents this object
     tcod.console_put_char(con=console,
                           x=entity.x, y=entity.y,
