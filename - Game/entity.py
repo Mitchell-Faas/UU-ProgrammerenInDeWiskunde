164422de196ef5e87166f7810f69189c1052cf4b
Mitchell <Faas.Mitchell@gmail.com>
2019-04-30 09:59:14 +0200

    added render function

---
+++ - Game/entity.py
@@ -0,0 +1,12 @@
+class Entity:
+    """"An object representing entities"""
+
+    def __init__(self, x, y, char, colour):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.colour = colour
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
