2356982ebc87a7cd94e15121f297ef3ee3fdb186
Mitchell Faas <Faas.Mitchell@gmail.com>
2019-05-03 00:54:44 +0200

    refactored rectangle.py

--- - Game/map_objects/rectangle.py
+++ - Game/map_objects/rectangle.py
@@ -1,16 +1,20 @@
 class Rect:
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, width, height):
         self.x1 = x
         self.y1 = y
-        self.x2 = x + w
-        self.y2 = y + h
+        self.x2 = x + width
+        self.y2 = y + height
 
     def center(self):
-        center_x = int((self.x1 + self.x2) / 2)
-        center_y = int((self.y1 + self.y2) / 2)
-        return (center_x, center_y)
+        """Calculates and returns the center coordinates of this rectangle"""
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+        return center_x, center_y
 
     def intersect(self, other):
-        # returns true if this rectangle intersects with another one
-        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-                self.y1 <= other.y2 and self.y2 >= other.y1)
+        # returns true if this rectangle intersects with "other"
+        intersect_bool = self.x1 <= other.x2 and \
+                         self.x2 >= other.x1 and \
+                         self.y1 <= other.y2 and \
+                         self.y2 >= other.y1
+        return intersect_bool
