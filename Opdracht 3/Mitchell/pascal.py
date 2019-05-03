8d1b18ea7727531a4464b3b7a56c8e336a80f364
Mitchell Faas <Faas.Mitchell@gmail.com>
2019-05-03 15:04:27 +0200

    Made pascal.py

---
+++ Opdracht 3/Mitchell/pascal.py
@@ -0,0 +1,16 @@
+def pascal(i, j):
+
+    # If we're on the edge, we return 1
+    if j == 0 or j == i:
+        return 1
+
+    # Start recursion
+    pascal_ij = pascal(i-1, j) + pascal(i-1, j-1)
+
+    return pascal_ij
+
+if __name__ == '__main__':
+    while True:
+        k, n = [int(x) for x in input().split()]
+
+        print(pascal(n-1, k-1))
