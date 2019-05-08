ea0e08273b15b89fa3a0c59b48644760de70f20d
Mitchell <Faas.Mitchell@gmail.com>
2019-05-08 12:48:44 +0200

    Added stringlist to individual column conversion

--- MTCS-Go/ai.py
+++ MTCS-Go/ai.py
@@ -1,13 +1,21 @@
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
 import os
 import sys
-
-import pandas as pd
+import ast
 
 # 81 input neurons
 # 2 output neurons
 
-path = os.path.join(sys.path[0], 'GameFiles/gamestates.csv')
+path = os.path.join(sys.path[0], 'GameFiles/gamestates2.csv')
+print(path)
 
 df = pd.DataFrame.from_csv(path)
+df['features'] = df["features"].apply(ast.literal_eval)
+print(type(df['features'][5]))
+df2 = pd.DataFrame(df['features'].tolist(), columns=['feature%d'%i for i in range(81)])
+df2['labels'] = df['labels']
 
-print(df)
+df2.to_csv(os.path.join(sys.path[0], 'gamestate2.csv'))
+print(df2)
