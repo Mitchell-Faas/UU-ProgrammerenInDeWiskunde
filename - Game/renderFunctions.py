0b2b485f3f64f0b4d1623675531c06b2f8ca1384
Pimski26 <p.w.j.terietmole@students.uu.nl>
2019-06-11 09:57:38 +0200

    Finished chapter 8 of tutorial

--- - Game/renderFunctions.py
+++ - Game/renderFunctions.py
@@ -114,9 +114,12 @@ def render_all(console, panel, entities, player, game_map, fov_map, fov_recomput
                       xdst=0, ydst=panelY)
 
     # Display inventory
-    if game_state == GameStates.SHOW_INVENTORY:
-        inventory_menu(console, 'Press the key next to an item to use it, or Esc to cancel.\n',
-                       player.inventory, 50, screen_width, screen_height)
+    if game_state in (GameStates.SHOW_INVENTORY,GameStates.DROP_INVENTORY):
+        if game_state == GameStates.SHOW_INVENTORY:
+            inventory_title = 'Press the key next to an item to use it, or Esc to cancel.\n'
+        else:
+            inventory_title = 'Press the key next to an item to drop it, or Esc to cancel.\n'
+        inventory_menu(console, inventory_title, player.inventory, 50, screen_width, screen_height)
 
 def clear_all(console, entities):
     """Removes all given entities from screen.
